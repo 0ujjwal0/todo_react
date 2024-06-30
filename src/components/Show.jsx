b9816f28313d6e333de0acc890aff5b881a0e772
ujjwal <90054010+0ujjwal0@users.noreply.github.com>
2024-06-30 22:46:32 +0530

    Update Show.jsx

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Modal from './Modal';
-import { useDrag, useDrop } from 'react-dnd'; // drag and drop feature
-import { updateTask } from '../store/tasksSlice';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Modal from "./Modal";
+import { useDrag, useDrop } from "react-dnd"; // drag and drop feature
+import { updateTask } from "../store/tasksSlice";
 
 const TaskList = () => {
   const tasks = useSelector((state) => state.tasks.tasks);
@@ -14,29 +14,45 @@ const TaskList = () => {
 
   const statuses = [
     { status: "notstarted", textColor: "text-white", bgColor: " bg-white" },
-    { status: "inprogress", textColor: "text-yellow-100", bgColor: "bg-yellow-100 border-solid border-1 border-yellow-600" },
-    { status: "completed", textColor: "text-green-100", bgColor: "bg-green-100 border-solid border-1 border-green-600" }
+    {
+      status: "inprogress",
+      textColor: "text-yellow-100",
+      bgColor: "bg-yellow-100 border-solid border-1 border-yellow-600",
+    },
+    {
+      status: "completed",
+      textColor: "text-green-100",
+      bgColor: "bg-green-100 border-solid border-1 border-green-600",
+    },
   ];
 
   return (
-    <div className='flex gap-40'>
-      {statuses.map(({ status, textColor, bgColor }, index) => (
-        <Section
-          key={index}
-          status={status}
-          tasks={tasks}
-          onTaskClick={handleTaskClick}
-          textColor={textColor}
-          bgColor={bgColor}
-        />
-      ))}
-      <Modal
-        onClose={() => setSelectedTask(null)}
-        isOpen={selectedTask !== null}
-        task={selectedTask}
-        setTask={setSelectedTask}
-      />
-    </div>
+    <>
+      {" "}
+      <div
+        className="bg-purple-100  w-screen 
+    flex flex-col items-center justify-between gap-20 pt-16"
+      >
+        <div className="flex flex-col sm:flex-row">
+          {statuses.map(({ status, textColor, bgColor }, index) => (
+            <Section
+              key={index}
+              status={status}
+              tasks={tasks}
+              onTaskClick={handleTaskClick}
+              textColor={textColor}
+              bgColor={bgColor}
+            />
+          ))}
+          <Modal
+            onClose={() => setSelectedTask(null)}
+            isOpen={selectedTask !== null}
+            task={selectedTask}
+            setTask={setSelectedTask}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
@@ -52,34 +68,40 @@ const Section = ({ status, tasks, onTaskClick, textColor, bgColor }) => {
     }),
   });
 
-  const filteredTasks = tasks.filter(task => task.status === status);
+  const filteredTasks = tasks.filter((task) => task.status === status);
 
   return (
-    <div ref={drop}
-      className={`w-64 rounded-md p-2 ${isOver ? "bg-opacity-75" : ""}`}
+    <div
+      ref={drop}
+      className={`w-full sm:w-64 rounded-md p-2 ${
+        isOver ? "bg-opacity-75" : ""
+      }`}
     >
-      <Header text={status} count={filteredTasks.length} textColor={textColor} />
-      {filteredTasks.length > 0 && filteredTasks.map((task) => (
-        <Task
-          key={task.id}
-          task={task}
-          onTaskClick={onTaskClick}
-          bgColor={bgColor}
-        />
-      ))}
+      <Header
+        text={status}
+        count={filteredTasks.length}
+        textColor={textColor}
+      />
+      {filteredTasks.length > 0 &&
+        filteredTasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onTaskClick={onTaskClick}
+            bgColor={bgColor}
+          />
+        ))}
     </div>
   );
 };
 
 const Header = ({ text, count, textColor }) => {
   return (
-    <div className="bg-purple-900 flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white">
+    <div className="bg-purple-900 flex-col md:flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white">
       <div className={`flex rounded-md pl-2 pr-2 text-xl ${textColor}`}>
         {text}
       </div>
-      <div className="ml-4 text-white">
-        {count}
-      </div>
+      <div className=" text-white">{count}</div>
     </div>
   );
 };
@@ -94,8 +116,11 @@ const Task = ({ task, onTaskClick, bgColor }) => {
   });
 
   return (
-    <div ref={drag}
-      className={`relative p-4 mt-3 shadow-md rounded-md hover:bg-gray-200 ${isDragging ? "opacity-10" : "opacity-100"} cursor-grab ${bgColor}`}
+    <div
+      ref={drag}
+      className={`relative p-4 mt-3 shadow-md rounded-md hover:bg-gray-200 ${
+        isDragging ? "opacity-10" : "opacity-100"
+      } cursor-grab ${bgColor}`}
       onClick={() => onTaskClick(task)}
     >
       <p>{task.name}</p>
