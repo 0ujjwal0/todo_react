b42077a96f749000b8f884913c44d8c56dbecaed
ujjwal <90054010+0ujjwal0@users.noreply.github.com>
2024-06-30 18:10:07 +0530

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+     return (
+        <div className="py-2 px-6 my-1 h-1/6 w-full  max-sm:px-2 max-sm:py-3 bg-purple-200 flex flex-row justify-between">
+        <div className="text-xl">
+        <h1 className="text-gray">This project was submitted to QUADB TECH</h1>
+        <p className="text-gray-700">by UJJWAL</p>
+        </div>
+        <div>
+        <div className="flex flex-row py-1 p-top-0">
+        <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=000000" alt="github icon" className="h-10 w-10"/>
+        <div className="text-gray" >github.com/0ujjwal0</div>
+        </div>
+        <div className="flex flex-row py-1">
+        <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="linkedn icon" className="h-10 w-10"/>
+        <div className="text-purple">linkedin.com/in/0ujjwal0/</div>
+        </div>
+        </div>
+        </div>
+      
+    );
+  };
+  
+  export default Footer;
+  
