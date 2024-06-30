a011c64675c028a3fadc46e29f07b48cb7b0e1c2
ujjwal <90054010+0ujjwal0@users.noreply.github.com>
2024-06-30 19:17:20 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,21 +1,27 @@
 const Footer = () => {
      return (
-        <div className="py-2 px-6 my-1 h-1/6 w-full  max-sm:px-2 max-sm:py-3 bg-purple-200 flex flex-row justify-between">
-        <div className="text-xl">
-        <h1 className="text-gray">This project was submitted to QUADB TECH</h1>
-        <p className="text-gray-700">by UJJWAL</p>
-        </div>
-        <div>
-        <div className="flex flex-row py-1 p-top-0">
-        <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=000000" alt="github icon" className="h-10 w-10"/>
-        <div className="text-gray" >github.com/0ujjwal0</div>
-        </div>
-        <div className="flex flex-row py-1">
-        <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="linkedn icon" className="h-10 w-10"/>
-        <div className="text-purple">linkedin.com/in/0ujjwal0/</div>
-        </div>
-        </div>
-        </div>
+        <footer className="mt-4 text-center text-gray-500 text-sm">
+        <p>This project is submitted to QUADB TECH</p>
+      <p>By Ujjwal ❤</p>
+      <div className="flex justify-center space-x-4 mt-2">
+        <a
+          href="https://www.linkedin.com/in/0ujjwal0/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:text-blue-800"
+        >
+          <img src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png&color=000000" className="h-10 w-10" alt="" />
+        </a>
+        <a
+          href="https://github.com/0ujjwal0"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-800 hover:text-gray-900"
+        >
+         <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=000000" className="h-10 w-10 "alt="" />
+        </a>
+      </div>
+      </footer>
       
     );
   };
